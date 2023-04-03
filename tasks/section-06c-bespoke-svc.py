375666f01f584338bfc7b2cd46aaaaee7f42c36f
Victor Ruto <vickruto@students.uonbi.ac.ke>
2023-04-03 13:26:30 +0000

    use standardize feature inputs from task 5b

--- tasks/section-06c-bespoke-svc.py
+++ tasks/section-06c-bespoke-svc.py
@@ -6,15 +6,16 @@ from pathlib import Path
 import pickle
 
 # %% tags=["parameters"]
-upstream = ['section-05-data-preparation']
+upstream = ['section-05b-standardize-features']
 product = None
 
 # %% tags=["soorgeon-unpickle"]
-X_train = pickle.loads(Path(upstream['section-05-data-preparation']['X_train']).read_bytes())
-X_val = pickle.loads(Path(upstream['section-05-data-preparation']['X_val']).read_bytes())
-y_train = pickle.loads(Path(upstream['section-05-data-preparation']['y_train']).read_bytes())
-y_val = pickle.loads(Path(upstream['section-05-data-preparation']['y_val']).read_bytes())
+X_train = pickle.loads(Path(upstream['section-05b-standardize-features']['X_train']).read_bytes())
+X_val = pickle.loads(Path(upstream['section-05b-standardize-features']['X_val']).read_bytes())
+y_train = pickle.loads(Path(upstream['section-05b-standardize-features']['y_train']).read_bytes())
+y_val = pickle.loads(Path(upstream['section-05b-standardize-features']['y_val']).read_bytes())
 
+# %%
 svm = SVC()
 svm.fit(X_train, y_train)
 print("Accuracy on the Train Dataset : ", svm.score(X_train, y_train))
--- tasks/section-06d-bespoke-logistic-regression.py
+++ tasks/section-06d-bespoke-logistic-regression.py
@@ -7,18 +7,18 @@ import pickle
 from sklearn.linear_model import LogisticRegression
 
 # %% tags=["parameters"]
-upstream = ['section-05-data-preparation']
+upstream = ['section-05b-standardize-features']
 product = None
 
 # %% tags=["soorgeon-unpickle"]
-X_train = pickle.loads(Path(upstream['section-05-data-preparation']['X_train']).read_bytes())
-X_val = pickle.loads(Path(upstream['section-05-data-preparation']['X_val']).read_bytes())
-y_train = pickle.loads(Path(upstream['section-05-data-preparation']['y_train']).read_bytes())
-y_val = pickle.loads(Path(upstream['section-05-data-preparation']['y_val']).read_bytes())
+X_train = pickle.loads(Path(upstream['section-05b-standardize-features']['X_train']).read_bytes())
+X_val = pickle.loads(Path(upstream['section-05b-standardize-features']['X_val']).read_bytes())
+y_train = pickle.loads(Path(upstream['section-05b-standardize-features']['y_train']).read_bytes())
+y_val = pickle.loads(Path(upstream['section-05b-standardize-features']['y_val']).read_bytes())
 
+# %%
 lr = LogisticRegression() 
 lr.fit(X_train, y_train)
 print("Accuracy on the Train Dataset : ", accuracy_score(lr.predict(X_train), y_train))
 print("Accuracy on the Validation Dataset : ", accuracy_score(lr.predict(X_val), y_val))
 print("\nConfusion matrix : \n", confusion_matrix(lr.predict(X_val), y_val))
-
